f37237ef834fc0f75c66e9a9f52695626265941d
Aitor Méndez <aitor@e451.net>
2023-12-10 14:05:51 +0100

    build

---
+++ .netlify/functions-internal/chunks/generic_1nb7g7K8.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/generic_Lu2XF4JS.mjs');
+
+export { page };
---
+++ .netlify/functions-internal/chunks/index_ukbbtxeJ.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/index_C90DZ_z5.mjs');
+
+export { page };
