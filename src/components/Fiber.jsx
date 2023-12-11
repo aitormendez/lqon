ae734003e66c2d2f682cda4543a77098069a1a3c
Aitor Méndez <aitor@e451.net>
2023-12-11 13:04:19 +0100

    torno (sin sombras)

--- src/components/Fiber.jsx
+++ src/components/Fiber.jsx
@@ -1,30 +1,55 @@
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import { CameraControls, Text, Float } from "@react-three/drei";
 import { useEffect, useRef } from "react";
-import { ACESFilmicToneMapping } from "three";
+import { ACESFilmicToneMapping, Euler } from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 const Scene = () => {
   const tornoTorno01 = useRef();
+  const tornoCuerpo01 = useRef();
 
   useEffect(() => {
     console.log(tornoTorno01);
-    // tornoTorno01.current.rotation.z = 1;
-    tornoTorno01.current.rotation.x = 1;
+    tornoTorno01.current.rotation.z = 2.36;
+    // tornoTorno01.current.rotation.x = 1;
   }, []);
 
   useFrame((state, delta) => {
-    tornoTorno01.current.rotation.y += delta;
+    tornoTorno01.current.rotateY(delta);
   });
   0;
 
   const tornoTorno = useLoader(GLTFLoader, "../models/torno-torno.glb");
+  const tornoCuerpo = useLoader(GLTFLoader, "../models/torno-cuerpo.glb");
 
   return (
     <>
-      <directionalLight position={[1, 2, 3]} />
+      <directionalLight castShadow position={[-1, 2, -2]} intensity={10} />
 
-      <primitive ref={tornoTorno01} object={tornoTorno.scene} scale={0.5} />
+      <primitive
+        castShadow
+        receiveShadow
+        ref={tornoTorno01}
+        object={tornoTorno.scene}
+        position={[-0.15, 0.8, 0]}
+        scale={0.5}
+      />
+
+      <primitive
+        castShadow
+        ref={tornoCuerpo01}
+        object={tornoCuerpo.scene}
+        scale={0.5}
+      />
+
+      <mesh
+        receiveShadow
+        rotation-x={-Math.PI * 0.5}
+        scale={100}
+      >
+        <planeGeometry />
+        <meshStandardMaterial color="white" />
+      </mesh>
 
       <CameraControls makeDefault />
     </>
