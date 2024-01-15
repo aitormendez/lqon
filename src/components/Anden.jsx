d0658e30c21cf76f08314b77951da0777f0da488
Aitor Méndez <aitor@e451.net>
2024-01-15 13:51:13 +0100

    andén: marco.plano baked

--- src/components/Anden.jsx
+++ src/components/Anden.jsx
@@ -50,44 +50,48 @@ export default function Model(props) {
       <mesh position={[250, 18, -14.8]} material={materials.negro}>
         <boxGeometry args={[100, 60, 40]} />
       </mesh>
+
       <mesh
         geometry={nodes.anden.geometry}
         material={materials["baked.suelo"]}
       />
-      <mesh
-        geometry={nodes.anden.geometry}
-        material={materials["baked.suelo"]}
-      />
+
       <mesh
         geometry={nodes.pared.geometry}
         material={materials["baked.pared"]}
         position={[0, 9.5, -10]}
       />
+
       <mesh
         geometry={nodes.pasillo.geometry}
         material={materials["baked.pasillo"]}
       />
+
       <mesh
         geometry={nodes.soportes.geometry}
         material={materials.negro}
         position={[86.607, 22.256, -55.019]}
       />
+
       <mesh geometry={nodes.cartelmarco.geometry} material={materials.negro} />
       <mesh
         geometry={nodes.cartelcartel001.geometry}
         material={materials["Cartel.002"]}
         position={[0, 0, 0.08]}
       />
+
       <mesh
         geometry={nodes.banco.geometry}
         material={materials.banco}
         position={[-151.306, 13.101, -8.752]}
       />
+
       <mesh
         geometry={nodes.mapamarco.geometry}
-        material={materials.blanco}
+        material={materials["marco.plano"]}
         position={[0, 0, -1.351]}
       />
+
       <mesh
         geometry={nodes.mapamapa001.geometry}
         material={materials["plano.002"]}
