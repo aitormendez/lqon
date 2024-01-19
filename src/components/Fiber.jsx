96273ab4902ec5afefdf3ac8cc1b43ec7227eb8e
Aitor Méndez <aitor@e451.net>
2024-01-19 14:38:09 +0100

    andén: trasladar lógica de la animación a Metro

--- src/components/Fiber.jsx
+++ src/components/Fiber.jsx
@@ -1,44 +1,19 @@
-import { useThree, Canvas, useFrame } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import { CameraControls } from "@react-three/drei";
-import { useCallback, useState, useEffect, useRef } from "react";
-import { ACESFilmicToneMapping } from "three";
-import Anden from "./Anden";
+import { useState } from "react";
 import Metro from "./Metro";
 
 export const Fiber = () => {
   const [isAnimating, setIsAnimating] = useState(false);
-  const [stations, setStations] = useState([
-    { id: 1, nombre: "Oscar Cabanas", position: [0, 0, 0] },
-  ]);
-  const [currentStationIndex, setCurrentStationIndex] = useState(0);
 
-  const estaciones = [
+  const stations = [
     { id: 1, nombre: "Oscar Cabanas" },
     { id: 2, nombre: "Encikutao" },
     { id: 3, nombre: "Taktel" },
   ];
 
-  const addStation = useCallback(() => {
-    const newStationIndex = (currentStationIndex + 1) % estaciones.length;
-    setCurrentStationIndex(newStationIndex);
-
-    const uniqueId = `station-${Date.now()}`;
-    const newPosition = [500, 0, 0];
-    const newStation = {
-      id: uniqueId,
-      nombre: estaciones[newStationIndex].nombre,
-      position: newPosition,
-    };
-    setStations((prevStations) => [...prevStations, newStation]);
-  }, [currentStationIndex, estaciones, setStations]);
-
   const handleAnimationStart = () => {
-    addStation(); // Cambia el nombre antes de iniciar la animación
     setIsAnimating(true);
-  };
-
-  const removeFirstStation = () => {
-    setStations(stations.slice(1));
   };
 
   return (
@@ -48,11 +23,9 @@ export const Fiber = () => {
         style={{ height: "30vw" }}
       >
         <Metro
+          stations={stations}
           isAnimating={isAnimating}
           setIsAnimating={setIsAnimating}
-          addStation={addStation}
-          removeFirstStation={removeFirstStation}
-          stations={stations}
         />
         {/* <CameraControls makeDefault /> */}
         <ambientLight intensity={2} />
--- src/components/Metro.jsx
+++ src/components/Metro.jsx
@@ -1,25 +1,48 @@
 import { useThree, useFrame } from "@react-three/fiber";
-import React, { useEffect, useRef } from "react";
+import { useCallback, useState, useEffect, useRef } from "react";
 import Anden from "./Anden";
 
-const Metro = ({
-  isAnimating,
-  setIsAnimating,
-  addStation,
-  removeFirstStation,
-  stations,
-}) => {
+const Metro = ({ isAnimating, setIsAnimating }) => {
   const { camera } = useThree();
   const isMoving = useRef(false);
   const startTimeRef = useRef(null);
+  const [currentStationIndex, setCurrentStationIndex] = useState(0);
+
+  const allStations = [
+    { id: 1, nombre: "Oscar Cabanas" },
+    { id: 2, nombre: "Encikutao" },
+    { id: 3, nombre: "Taktel" },
+  ];
+
+  const [stations, setStations] = useState([
+    { id: 1, nombre: "Oscar Cabanas", position: [0, 0, 0] },
+  ]);
+
+  const addStation = useCallback(() => {
+    const newStationIndex = (currentStationIndex + 1) % allStations.length;
+    setCurrentStationIndex(newStationIndex);
+
+    const uniqueId = `station-${Date.now()}`;
+    const newPosition = [500, 0, 0];
+    const newStation = {
+      id: uniqueId,
+      nombre: allStations[newStationIndex].nombre,
+      position: newPosition,
+    };
+    setStations((prevStations) => [...prevStations, newStation]);
+  }, [currentStationIndex, allStations]);
+
+  const removeFirstStation = () => {
+    setStations(stations.slice(1));
+  };
 
   useEffect(() => {
     if (isAnimating && !isMoving.current) {
-      addStation(); // Añade una nueva estación
+      addStation();
       startTimeRef.current = null; // Reinicia el tiempo de inicio
       isMoving.current = true; // Comienza el movimiento del tren
     }
-  }, [isAnimating, addStation]);
+  }, [isAnimating]);
 
   useFrame(({ clock }) => {
     if (isMoving.current) {
@@ -56,9 +79,12 @@ const Metro = ({
     }
   });
 
+  const handleAnimationStart = () => {
+    setIsAnimating(true);
+  };
+
   return (
     <>
-      <Anden scale={1} position={[0, 0, 0]} />
       {stations.map((station) => (
         <Anden
           key={station.id}
