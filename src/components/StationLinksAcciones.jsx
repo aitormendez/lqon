f1a400cf930d642ab1935e1806e1d4064f8d441d
Aitor Méndez <aitor@e451.net>
2024-02-05 08:38:06 +0100

    solapa CSS texto proporcional

--- src/components/StationLinksAcciones.jsx
+++ src/components/StationLinksAcciones.jsx
@@ -33,7 +33,7 @@ const StationLinks = () => {
     <>
       {accionStations.map((station, index) => (
         <a
-          className="block px-6 border-b py-2 text-white hover:text-red-600 last:border-none"
+          className="block px-6 md:px-20 border-b py-2 text-white hover:text-red-600 last:border-none"
           key={`${station.nombre}-${index}`}
           href={`/acciones/${station.uri}`}
           onClick={(e) => goToStation(station.uri, e)}
